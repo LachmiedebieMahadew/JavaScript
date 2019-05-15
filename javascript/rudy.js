ef4da9abb3a55d36e42d71c4952ae39d8debf63b
Lachmie Mahadew <kashish.lachmie@gmail.com>
2019-05-15 15:15:33 -0500

    index, lab1 homework1 homework2 grade Exercise homework4 added calculator lab to index added servlet quiz Saturday lab done finished all labs before midterm javscript exercises

--- javascript/rudy.js
+++ javascript/rudy.js
@@ -9,7 +9,7 @@ var accountList= [];
      accountList["Savings"]=accountList["savings"]= accountList["SAVINGS"]="Savings";
      accountList["Business"]=accountList["business"]= accountList["BUSINESS"]="business";
 let  numberOfAccount=0;
-let amount=0.0;
+
 var rudyTimer = ( function(){
    var timer= null;
    function delayMsg2(){
@@ -29,59 +29,71 @@ var rudyTimer = ( function(){
     
 })();
 
- var account= {
-    accountName:"",
-    balance:0.0,
-    amount:0.0,
-    name: function(){return "AccountName:"+ this.accountName;},
-  deposit:function(){ this.balance= this.balance+ this.amount;}  
+// var account= {
+//    accountName:"",
+//    balance:0.0,
+//    amount:0.0,
+//    name: function(){return "AccountName:"+ this.accountName;},
+//  deposit:function(){ this.balance= this.balance+ this.amount;}  
+//};
+//
+// var checkingAccount= function(amount){
+//     let checkAcct= Object.create(account);
+//     checkAcct.accountName="Checking",
+//     checkAcct.amount=amount;
+//     checkAcct.balance= account.deposit;
+//     return checkAcct;
+//  };
+// 
+// var SavingsAccount= function(amount){
+//     let SavingsAcct= Object.create(account);
+//     SavingsAcct.accountName="Savings",
+//     SavingsAcct.amount=amount;
+//     SavingsAcct.balance= account.deposit;
+//     return SavingsAcct;
+//  };
+//  var BusinessAccount= function(amount){
+//     let BusinessAcct= Object.create(account);
+//     BusinessAcct.accountName="Business",
+//     BusinessAcct.amount=amount;
+//     BusinessAcct.balance= account.deposit;
+//     return BusinessAcct;
+//  };
+
+function Account( name, amount){
+    let balance=0.0;
+    this.name= name;
+    this.amount= amount;
+    this. balance= this.balance+ amount;   
 };
-
- var checkingAccount= function(amount){
-     let checkAcct= Object.create(account);
-     checkAcct.accountName="Checking",
-     checkAcct.amount=amount;
-     checkAcct.balance= account.deposit;
-     return checkAcct;
-  };
- 
- var SavingsAccount= function(amount){
-     let SavingsAcct= Object.create(account);
-     SavingsAcct.accountName="Savings",
-     SavingsAcct.amount=amount;
-     SavingsAcct.balance= account.deposit;
-     return SavingsAcct;
-  };
-  var BusinessAccount= function(amount){
-     let BusinessAcct= Object.create(account);
-     BusinessAcct.accountName="Business",
-     BusinessAcct.amount=amount;
-     BusinessAcct.balance= account.deposit;
-     return BusinessAcct;
-  };
-
 var createAccount= function(){
-   let acctName= document.getElementById("Accountname");
+    let acctName= document.getElementById("Accountname");
    let money= parseDouble(document.getElementById("amount").value);
-   var newAccount;
-   account.balance= money;
-   //this.amount=money;
-   switch(acctName.value){
-       case"Checking","CHECKING","checking":newAccount= new checkingAccount("Checking", amount); break;
-       case"Savings","SAVINGS","savings":newAccount=  new SavingsAccount("Savings",amount); break;
-       case"Business","BUSINESS","business": newAccount=  new BusinessAccount("Business",amount); break;
-   }
-    numberOfAccount++;
-   //alert( numberOfAccount);
-   alert( newAccount.name);
-   return newAccount;
+    let newAccount;
+    switch(acctName.value){
+       case"Checking","CHECKING","checking":newAccount= new Account("Checking", money); break;
+       case"Savings","SAVINGS","savings":newAccount=  new Account("Savings", money); break;
+       case"Business","BUSINESS","business": newAccount=  new Account("Business",money); break; 
+    }
+    alert(newAccount.name +":" + newAccount.balance);
+    return newAccount;
 };
- var AccountInfo= function(){
-      let accounts=[];
-      
-      
-     
- };
+//   let acctName= document.getElementById("Accountname");
+//   let money= parseDouble(document.getElementById("amount").value);
+//   var newAccount;
+//   account.balance= money;
+//   //this.amount=money;
+//   switch(acctName.value){
+//       case"Checking","CHECKING","checking":newAccount= new checkingAccount("Checking", amount); break;
+//       case"Savings","SAVINGS","savings":newAccount=  new SavingsAccount("Savings",amount); break;
+//       case"Business","BUSINESS","business": newAccount=  new BusinessAccount("Business",amount); break;
+//   }
+//    numberOfAccount++;
+//   //alert( numberOfAccount);
+//   alert( newAccount.name);
+//   return newAccount;
+
+//};
 
 window.onload = function (){
    let button= document.getElementById("btn");
