9f87d03bf74e93d074921db364c840e8c3ea5cc2
Lachmie Mahadew <kashish.lachmie@gmail.com>
2019-05-16 16:37:06 -0500

    index, lab1 homework1 homework2 grade Exercise homework4 added calculator lab to index added servlet quiz Saturday lab done finished all labs before midterm javscript exercises

---
+++ javascript/maze.js
@@ -0,0 +1,72 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+
+   $(document).ready( function () {
+      var count=0;
+    $("#start").click(function () {
+         $("#status").text("Game is running....");
+        $(".boundary").mouseover(function() {
+            $(this).css({"background-color":"red"});
+             count++;
+           $(".boundary.example").text("Boundary hit:"+count);
+           $("#status").text("You lost");
+           alert("you lost");
+        });
+         
+       $("#end").mouseover(function(){
+           if(count===0){
+               $(".boundary.example").text("Boundary hit:"+count);
+               $("#status").text("congrats, You won");
+               alert("you win");
+               location.reload();
+           }
+           else {
+               $(".boundary.example").text("Boundary hit:"+count);
+               $("#status").text("Sorry, you lost");
+               alert("you lost");
+               location.reload();
+           }
+       });
+       
+    });
+       
+       
+   });   
+    
+   
+       
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
